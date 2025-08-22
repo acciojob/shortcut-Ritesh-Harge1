bc64aa86a43603e47352bcba0f0f1dc55addab1c
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-22 14:06:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function shortcut(s1, s2) {
+    if (!s1 || !s2) return "";
+    return s1[0] + s2[0];
+}
 
-let input = [];
+let str1 = prompt("Enter first string:");
+let str2 = prompt("Enter second string:");
 
-rl.on("line", function(line) {
-    input.push(line.trim());
-}).on("close", function() {
-    let str1 = input[0] || "";
-    let str2 = input[1] || "";
-
-    function shortcut(s1, s2) {
-        if (!s1 || !s2) return "";
-        return s1[0] + s2[0];
-    }
-
-    console.log(shortcut(str1, str2));
-});
+alert(shortcut(str1, str2));
 
 
