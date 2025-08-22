6bffad58de9ff5b5592318ba245716f0c340bee1
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-22 14:01:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ const rl = readline.createInterface({
 });
 
 let input = [];
+
 rl.on("line", function(line) {
     input.push(line.trim());
 }).on("close", function() {
-    let str1 = input[0] || "";
-    let str2 = input[1] || "";
+    let [str1, str2] = input;   // read two strings
 
-    function shortcut(a, b) {
-        if (!a || !b) return "";
-        return a[0] + b[0];
+    function shortcut(s1, s2) {
+        if (!s1 || !s2) return "";
+        return s1[0] + s2[0];
     }
 
     console.log(shortcut(str1, str2));
 });
+
 
