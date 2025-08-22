9ba7b12247c466029cb8198eafb72690c8edca6b
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-22 14:04:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ let input = [];
 rl.on("line", function(line) {
     input.push(line.trim());
 }).on("close", function() {
-    let [str1, str2] = input;   // read two strings
+    let str1 = input[0] || "";
+    let str2 = input[1] || "";
 
     function shortcut(s1, s2) {
         if (!s1 || !s2) return "";
